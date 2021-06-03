b5207669697c7242f21d66fb737186af6da9aa33
wpbindt <wesselbindt@gmail.com>
2021-06-03 22:38:36 +0200

    Implement stupid first go

--- tests.py
+++ tests.py
@@ -2,52 +2,61 @@ from wood_chipper import break_up_source_file, SourceFile
 
 
 def test_case(source_file, expected):
+    actual = break_up_source_file(source_file)
     assert break_up_source_file(source_file) == expected
 
 
 source_file = SourceFile(
     'filename.py',
-    'from dataclasses import dataclass\n'
-    'import os\n'
-    '\n'
-    '\n'
-    '@dataclass\n'
-    'class HelloWorldSayer:\n'
-    '    user: str\n'
-    '\n'
-    '    def say(self, something: str) -> str:\n'
-    '        return f"Hi, {self.user}, {something}"\n'
-    '\n'
-    '\n'
-    'class HelloWorld:\n'
-    '    def hi(self):\n'
-    '        print(f"hello, {os.name}")\n'
-    '\n'
+    (
+        'from dataclasses import dataclass',
+        'import os',
+        '',
+        '',
+        '@dataclass',
+        'class HelloWorldSayer:',
+        '    user: str',
+        '',
+        '    def say(self, something: str) -> str:',
+        '        return f"Hi, {self.user}, {something}"',
+        '',
+        '',
+        'class HelloWorld:',
+        '    def hi(self):',
+        '        print(f"hello, {os.name}")',
+        '',
+    )
 )
 
 expected_1 = SourceFile(
     'hello_world_sayer.py',
-    'from dataclasses import dataclass\n'
-    '\n'
-    '\n'
-    '@dataclass\n'
-    'class HelloWorldSayer:\n'
-    '    user: str\n'
-    '\n'
-    '    def say(self, something: str) -> str:\n'
-    '        return f"Hi, {self.user}, {something}"\n'
-    '\n'
+    (
+        'from dataclasses import dataclass',
+        'import os',
+        '',
+        '',
+        '@dataclass',
+        'class HelloWorldSayer:',
+        '    user: str',
+        '',
+        '    def say(self, something: str) -> str:',
+        '        return f"Hi, {self.user}, {something}"',
+        '',
+    )
 )
 
 expected_2 = SourceFile(
     'hello_world.py',
-    'import os\n'
-    '\n'
-    '\n'
-    'class HelloWorld:\n'
-    '    def hi(self):\n'
-    '        print("hello, {os.name}")\n'
-    '\n'
+    (
+        'from dataclasses import dataclass',
+        'import os',
+        '',
+        '',
+        'class HelloWorld:',
+        '    def hi(self):',
+        '        print(f"hello, {os.name}")',
+        '',
+    )
 )
 
 test_case(source_file, {expected_1, expected_2})
--- wood_chipper.py
+++ wood_chipper.py
@@ -1,12 +1,58 @@
+import ast
 from dataclasses import dataclass
+import re
 
 
-@dataclass
+@dataclass(frozen=True)
 class SourceFile:
     filename: str
-    contents: str
+    lines: tuple[str]
+
+    def parse(self) -> ast.Module:
+        return ast.parse('\n'.join(self.lines), self.filename)
+
+
+def get_import_lines(syntax_tree: ast.Module) -> list[int]:
+    return [
+        line_number
+        for node in syntax_tree.body
+        for line_number in range(node.lineno - 1, node.end_lineno)  # ast has 1-indexed line numbers
+        if isinstance(node, (ast.Import, ast.ImportFrom))
+    ]
+
+
+def extract_imports(code: SourceFile) -> list[str]:
+    syntax_tree = code.parse()
+    import_lines = get_import_lines(syntax_tree)
+    return [
+        code.lines[import_lineno]
+        for import_lineno in import_lines
+    ]
+
+
+def camel_to_snake(name: str) -> str:
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
 
 def break_up_source_file(code: SourceFile) -> set[SourceFile]:
-    ...
+    output = set()
+    imports = extract_imports(code)
+    non_imports = [
+        node
+        for node in code.parse().body
+        if not isinstance(node, (ast.Import, ast.ImportFrom))
+    ]
+    for non_import in non_imports:
+        output.add(SourceFile(
+            lines=tuple([
+                *imports,
+                '',
+                '',
+                *code.lines[non_import.lineno - 1 - len(non_import.decorator_list): non_import.end_lineno],
+                '',
+            ]),
+            filename=camel_to_snake(non_import.name) + '.py'
+        ))
 
+    return output
