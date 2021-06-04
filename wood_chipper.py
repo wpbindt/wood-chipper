f8437380148cf6545ff3bcfbd8181d778590b5b5
wpbindt <wesselbindt@gmail.com>
2021-06-04 12:18:50 +0200

    Rewrite structure

--- wood_chipper.py
+++ wood_chipper.py
@@ -1,58 +1,51 @@
-import ast
+# parse file to SourceFile object (filename and source lines)
+# convert SourceFile to ast object
+# extract the imports from the sourcefile object (tuple of str)
+# extract non-imports from SourceFile object, whose name is snake case version of class name (set of sourcefile objects)
+# compile list of imports of newly created files
+# combine the above 3 items in sourcefiles
+# write all SourceFile object to actual files in new module named after original file
+# purge unused imports
+# clean code style for imports
+# include imports in __init__.py
+
+# SOMEWHERE:
+# deal with comments
+
+from __future__ import annotations
+from ast import AST
 from dataclasses import dataclass
-import re
 
 
 @dataclass(frozen=True)
 class SourceFile:
     filename: str
     lines: tuple[str]
-
-    def parse(self) -> ast.Module:
-        return ast.parse('\n'.join(self.lines), self.filename)
-
-
-def get_import_lines(syntax_tree: ast.Module) -> list[int]:
-    return [
-        line_number
-        for node in syntax_tree.body
-        for line_number in range(node.lineno - 1, node.end_lineno)  # ast has 1-indexed line numbers
-        if isinstance(node, (ast.Import, ast.ImportFrom))
-    ]
+        
+    @classmethod
+    def from_file(cls, path: str) -> SourceFile:
+        ...
+        
+    def parse(self) -> AST:
+        ...
+        
+    def write(self, path: Path) -> None:
+        ...
 
 
-def extract_imports(code: SourceFile) -> list[str]:
-    syntax_tree = code.parse()
-    import_lines = get_import_lines(syntax_tree)
-    return [
-        code.lines[import_lineno]
-        for import_lineno in import_lines
-    ]
+def imports(source_file: SourceFile) -> tuple[str]:
+    ...
 
 
-def camel_to_snake(name: str) -> str:
-    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+def non_imports(source_file: SourceFile) -> set[SourceFile]:
+    ...
+    
 
+def add_imports(source_file: SourceFile, imports: tuple[str]) -> SourceFile:
+    ...
+    
+    
+def purge_unused_imports(source_file: SourceFile) -> SourceFile:
+    # auto_flake
+    ...
 
-def break_up_source_file(code: SourceFile) -> set[SourceFile]:
-    output = set()
-    imports = extract_imports(code)
-    non_imports = [
-        node
-        for node in code.parse().body
-        if not isinstance(node, (ast.Import, ast.ImportFrom))
-    ]
-    for non_import in non_imports:
-        output.add(SourceFile(
-            lines=tuple([
-                *imports,
-                '',
-                '',
-                *code.lines[non_import.lineno - 1 - len(non_import.decorator_list): non_import.end_lineno],
-                '',
-            ]),
-            filename=camel_to_snake(non_import.name) + '.py'
-        ))
-
-    return output
