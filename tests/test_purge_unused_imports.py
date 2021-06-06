04f717781ab545ed23c87bdb469c7f2180c01376
wpbindt <wesselbindt@gmail.com>
2021-06-06 23:31:42 +0200

    Add test for purge_unused_imports, but really

---
+++ tests/test_purge_unused_imports.py
@@ -0,0 +1,24 @@
+from source_file import SourceFile
+from wood_chipper import purge_unused_imports
+
+
+def test_purge_unused_imports(source_file_info) -> None:
+    source_file, imports, _ = source_file_info
+    actual = purge_unused_imports(source_file_info)
+
+    unused_imports = (
+        'import this',
+        'import that',
+        'from such import so, da, \\',
+        '    fountain',
+    )
+    expected = SourceFile(
+        filename=source_file.filename,
+        lines=tuple(
+            line
+            for line in source_file.lines
+            if line not in unused_imports
+        )
+    )
+
+    assert actual == expected
